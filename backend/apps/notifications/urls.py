cfe9c83c006d2b18ad379996b98471b8e2182fae
EL-HOUSS-BRAHIM <brahim.crafts.tech@gmail.com>
2025-07-21 01:09:20 +0100

    updating: urls

--- backend/apps/notifications/urls.py
+++ backend/apps/notifications/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from . import views
 
 app_name = 'notifications'
 
 urlpatterns = [
-    # Notification endpoints will be added here
+    # Basic notification endpoint
+    path('', views.NotificationListView.as_view(), name='notification_list'),
 ]
