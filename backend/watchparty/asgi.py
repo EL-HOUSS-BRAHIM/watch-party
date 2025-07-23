67cd88f37dba97cdcbe831c1b3a44002d937f897
EL-HOUSS-BRAHIM <brahim.crafts.tech@gmail.com>
2025-07-23 18:25:13 +0100

    updating: asgi

--- backend/watchparty/asgi.py
+++ backend/watchparty/asgi.py
@@ -18,19 +18,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'watchparty.settings.development
 django_asgi_app = get_asgi_application()
 
 # Import routing after Django is set up
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-from apps.chat.routing import websocket_urlpatterns as chat_websocket_urlpatterns
-from apps.interactive.routing import websocket_urlpatterns as interactive_websocket_urlpatterns
+from .routing import application as routing_application
 
-# Combine all WebSocket URL patterns
-all_websocket_urlpatterns = chat_websocket_urlpatterns + interactive_websocket_urlpatterns
-
-application = ProtocolTypeRouter({
-    "http": django_asgi_app,
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            all_websocket_urlpatterns
-        )
-    ),
-})
+application = routing_application
