22b369c1b970d4d109cb09c29d9493cd93ce51ac
EL-HOUSS-BRAHIM <brahim.crafts.tech@gmail.com>
2025-07-30 21:02:01 +0100

    updating: page

---
+++ frontend/app/join/page.tsx
@@ -0,0 +1,11 @@
+import { JoinParty } from "@/components/party/join-party"
+
+export default function JoinPage() {
+  return (
+    <div className="container mx-auto py-8">
+      <div className="max-w-md mx-auto">
+        <JoinParty />
+      </div>
+    </div>
+  )
+}
