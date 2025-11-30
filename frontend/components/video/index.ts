96e5396abba2b76e6c5c13da668181bce77f3a77
BRAHIM EL HOUSS <brahim.crafts.tech@gmail.com>
2025-11-30 22:20:21 +0100

    Add: index

---
+++ frontend/components/video/index.ts
@@ -0,0 +1,9 @@
+// Video components
+export { VideoCard } from "./VideoCard"
+export { GDriveFileCard } from "./GDriveFileCard"
+export { UploadSection } from "./UploadSection"
+export { VideoSearchFilter } from "./VideoSearchFilter"
+export { default as VideoPlayer } from "./VideoPlayer"
+
+// Utilities
+export * from "./video-utils"
