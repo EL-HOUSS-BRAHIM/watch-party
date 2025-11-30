b5e6f36dd4a4262d2de43e89e5dd0195cda0a7e1
BRAHIM EL HOUSS <brahim.crafts.tech@gmail.com>
2025-11-30 10:54:27 +0100

    Fix: Allow all frame-ancestors for VS Code Simple Browser

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -62,7 +62,7 @@ const nextConfig = {
               "object-src 'none'",
               "base-uri 'self'",
               "form-action 'self'",
-              "frame-ancestors 'self' https://*.vscode-cdn.net vscode-webview://*",
+              "frame-ancestors *",
               "upgrade-insecure-requests"
             ].join('; ')
           },
